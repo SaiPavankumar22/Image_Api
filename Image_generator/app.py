6c2fe0cf5b681487c2a4cdaaf3414b2acde69509
DEVISETTI SAI PAVAN KUMAR <devisettisaipavankumar@gmail.com>
2025-02-01 12:35:02 +0530

    Add files via upload

---
+++ Image_generator/app.py
@@ -0,0 +1,35 @@
+# app.py
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+ACCESS_KEY = "Tks2HeM3odm5pmjYnb_IDpNwXZtpN07nheWDpJoWt8M"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form.get('keyword')
+    url = "https://api.unsplash.com/search/photos"
+    params = {
+        "query": query,
+        "client_id": ACCESS_KEY,
+        "per_page": 1
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data["results"]:
+            image_url = data["results"][0]["urls"]["regular"]
+            image_link = data["results"][0]["links"]["html"]
+            return jsonify({"image_url": image_url, "image_link": image_link})
+        else:
+            return jsonify({"error": "No images found."})
+    else:
+        return jsonify({"error": "Error fetching data from Unsplash."})
+
+if __name__ == '__main__':
+    app.run(debug=True)
